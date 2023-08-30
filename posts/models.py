3bc931d31d8636ee74058fb84d0fe612ddcd4a7b
georgetoloraia <gtoloraia19@gmail.com>
2023-08-30 23:14:30 +0400

    updated models, views, serializers... (def create_post, Post model..)

--- posts/models.py
+++ posts/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from hashtags.model import Hashtag
 
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    hashtags = models.ManyToManyField(Hashtag, related_name='posts')
 
     def __str__(self):
         return (f"{self.user} - {self.title}")

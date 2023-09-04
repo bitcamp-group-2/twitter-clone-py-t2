aca14f30d0856351cb39e4ceb7600e8af008c6cc
BaCho <bachana.sulava@gmail.com>
2023-09-04 22:43:00 +0400

    minor error fixes

--- twitter/urls.py
+++ twitter/urls.py
@@ -18,9 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # path('', include('posts.urls')),
-    path('home/', include('registracion.urls')),
-    # path('', include("message.urls")),
     path('', include("groups.urls")), #BaCho
+    path('admin/', admin.site.urls),
+    path('', include('posts.urls')),
+    path('home/', include('registracion.urls')),
+    path('', include("message.urls")),
+    
 ]
